ce26249008dbc75d2178b4923ad56610119552a6
Avi Rana <itzavi1718@gmail.com>
2025-09-17 23:40:44 +0530

    Update TopNav.tsx

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -7,7 +7,7 @@ export function TopNav() {
       <h1 className="text-2xl">Nook</h1>
       <ul className="flex gap-4 justify-center items-center">
         <li><Upload /></li>
-        <li><Link to="/Search">Avi</Link></li>
+        <li><Link to="/auth">Avi</Link></li>
       </ul>
     </div>
   )
